c1f77b19387c324f47ba73efc7327786645b4f2c
Cameron <32912464+kiwi-cam@users.noreply.github.com>
2020-06-11 11:49:35 +1200

    Extended RM4 Headers to all RM4 devices

--- index.js
+++ index.js
@@ -251,8 +251,9 @@ class Device {
     this.type = deviceType;
     this.model = rmDeviceTypes[parseInt(deviceType, 16)] || rmPlusDeviceTypes[parseInt(deviceType, 16)] || rm4DeviceTypes[parseInt(deviceType, 16)];
 
-    this.request_header = parseInt(deviceType, 16) === parseInt(0x5f36, 16) ? new Buffer([0x04, 0x00]) : new Buffer([]);
-    this.code_sending_header = parseInt(deviceType, 16) === parseInt(0x5f36, 16) ? new Buffer([0xd0, 0x00]) : new Buffer([]);
+    //Use different headers for rm4 devices
+    this.request_header = rm4DeviceTypes[parseInt(deviceType, 16)] ? new Buffer([0x04, 0x00]) : new Buffer([]);
+    this.code_sending_header = rm4DeviceTypes[parseInt(deviceType, 16)] ? new Buffer([0xd0, 0x00]) : new Buffer([]);
 
     this.on = this.emitter.on;
     this.emit = this.emitter.emit;
