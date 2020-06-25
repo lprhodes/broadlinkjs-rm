f42b52a383ddeba9418028656e953ba8532e2f41
Cameron <32912464+kiwi-cam@users.noreply.github.com>
2020-06-26 09:42:13 +1200

    Adds Broadlink RM Mini 3 KR support

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ rmDeviceTypes[parseInt(0x27c7, 16)] = 'Broadlink RM Mini 3 A';
 rmDeviceTypes[parseInt(0x27c2, 16)] = "Broadlink RM Mini 3 B";
 rmDeviceTypes[parseInt(0x27de, 16)] = "Broadlink RM Mini 3 C";
 rmDeviceTypes[parseInt(0x5f36, 16)] = "Broadlink RM Mini 3 D";
+rmDeviceTypes[parseInt(0x27d3, 16)] = "Broadlink RM Mini 3 KR";
 rmDeviceTypes[parseInt(0x273d, 16)] = 'Broadlink RM Pro Phicomm';
 rmDeviceTypes[parseInt(0x2712, 16)] = 'Broadlink RM2';
 rmDeviceTypes[parseInt(0x2783, 16)] = 'Broadlink RM2 Home Plus';
